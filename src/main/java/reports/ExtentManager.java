529e3175b911c1a3bd04c3a7680a3832f6af6493
Teodora Vermesan <vermesanteodora@gmail.com>
2025-07-26 15:48:08 +0300

    refactor

---
+++ src/main/java/reports/ExtentManager.java
@@ -0,0 +1,16 @@
+package utils;
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+            extent = new ExtentReports();
+            extent.attachReporter(sparkReporter);
+        }
+        return extent;
+    }
+}
